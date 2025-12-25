b7b725ca334ed932e76c0a34c8b1d55f613c0984
vadymkz <vadim.kuznyetsov@gmail.com>
2025-12-25 23:44:17 +0200

    finished dou scraper

--- settings.py
+++ settings.py
@@ -3,7 +3,14 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 EXCLUDE_FROM_TITLE = [
-    'C#', 'Machine', 'AI', 'ML', 'LLM', 'Odoo', 'GCP', 'Angular', 'QA', 'Part-time', 'short-term', 'Cloud', 'Big Data',
-    'Science', 'Head', 'Lead', 'Node', 'Java', 'drone', 'trainee', 'junior'
+    'c#', 'с#', 'Machine', 'AI', 'ML', 'LLM', 'Odoo', 'GCP', 'Angular', 'QA', 'Part-time', 'short-term', 'Cloud',
+    'Science', 'Head', 'Lead', 'Node', 'Java', 'drone', 'trainee', 'junior', 'Leader', 'Principal', 'test', 'DevOps',
+    'c++', 'с++', 'math', 'Platform', 'Algorithms', 'Kubernetes', 'Architect', 'Intern', 'NumPy', 'Vision',
+    'UAE', 'Yii', 'Embedded', 'Big Data', 'office', 'Vue.JS', 'Azure'
 ]
 EXCLUDE_FROM_TITLE = {s.lower() for s in EXCLUDE_FROM_TITLE}
+
+EXCLUDE_FROM_COMPANY_NAME = ['ajax']
+EXCLUDE_FROM_COMPANY_NAME = {s.lower() for s in EXCLUDE_FROM_COMPANY_NAME}
+
+MINIMAL_SALARY = 3000
