fcd179cb2aab44bd7d175019b55f3f91bb0f5b6b
vadymkz <vadim.kuznyetsov@gmail.com>
2026-01-13 11:54:54 +0200

    changed settings and filters

--- settings.py
+++ settings.py
@@ -6,11 +6,12 @@ EXCLUDE_FROM_TITLE = [
     'c#', 'с#', 'Machine', 'AI', 'ML', 'LLM', 'Odoo', 'GCP', 'Angular', 'QA', 'Part-time', 'short-term', 'Cloud',
     'Science', 'Head', 'Lead', 'Node', 'Java', 'drone', 'trainee', 'junior', 'Leader', 'Principal', 'test', 'DevOps',
     'c++', 'с++', 'math', 'Platform', 'Algorithms', 'Kubernetes', 'Architect', 'Intern', 'NumPy', 'Vision',
-    'UAE', 'Yii', 'Embedded', 'Big Data', 'office', 'Vue.JS', 'Azure', 'freelance', 'analyst', 'Go'
+    'UAE', 'Yii', 'Embedded', 'Big Data', 'office', 'Vue.JS', 'Azure', 'freelance', 'analyst', 'Go', 'Robotics', 'rust',
+    'Airflow', 'Golang', 'React'
 ]
 EXCLUDE_FROM_TITLE = {s.lower() for s in EXCLUDE_FROM_TITLE}
 
-EXCLUDE_FROM_COMPANY_NAME = ['ajax', 'моу', 'Defence', 'Камера', 'ЗАЗМІК', 'Poklyk']
+EXCLUDE_FROM_COMPANY_NAME = ['ajax', 'моу', 'Defence', 'Камера', 'ЗАЗМІК', 'Warbirds']
 EXCLUDE_FROM_COMPANY_NAME = {s.lower() for s in EXCLUDE_FROM_COMPANY_NAME}
 
 MINIMAL_SALARY = 3000
