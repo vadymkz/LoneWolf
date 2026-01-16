561745cc172f0877a54cc8249aa4e420ecb07613
vadymkz <vadim.kuznyetsov@gmail.com>
2026-01-16 18:51:32 +0200

    moved settings data to env file

--- settings.py
+++ settings.py
@@ -1,21 +1,25 @@
 import os
 
+
+def fix_mojibake(s: str) -> str:
+    try:
+        return s.encode("cp1251").decode("utf-8")
+    except UnicodeError:
+        return s
+
+
+def get_list_env_var(env_name: str) -> list:
+    raw = fix_mojibake(os.getenv(env_name, "")).split(",")
+    return [v.strip().casefold().lower() for v in raw if v]
+
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-EXCLUDE_FROM_TITLE = [
-    'c#', 'с#', 'Machine', 'AI', 'ML', 'LLM', 'Odoo', 'GCP', 'Angular', 'QA', 'Part-time', 'short-term', 'Cloud',
-    'Science', 'Head', 'Lead', 'Node', 'Java', 'drone', 'trainee', 'junior', 'Leader', 'Principal', 'test', 'DevOps',
-    'c++', 'с++', 'math', 'Platform', 'Algorithms', 'Kubernetes', 'Architect', 'Intern', 'NumPy', 'Vision',
-    'UAE', 'Yii', 'Embedded', 'Big Data', 'office', 'Vue.JS', 'Azure', 'freelance', 'analyst', 'Go', 'Robotics', 'rust',
-    'Airflow', 'Golang', 'React', 'PySpark', 'Databricks', 'AQA', 'Front end'
-]
-EXCLUDE_FROM_TITLE = {s.lower() for s in EXCLUDE_FROM_TITLE}
+EXCLUDE_FROM_TITLE = set(get_list_env_var("EXCLUDE_FROM_TITLE"))
+EXCLUDE_FROM_COMPANY_NAME = set(get_list_env_var("EXCLUDE_FROM_COMPANY_NAME"))
 
-EXCLUDE_FROM_COMPANY_NAME = ['ajax', 'моу', 'Defence', 'Камера', 'ЗАЗМІК', 'Warbirds', 'softserve']
-EXCLUDE_FROM_COMPANY_NAME = {s.lower() for s in EXCLUDE_FROM_COMPANY_NAME}
+MINIMAL_SALARY = int(os.getenv("MIN_SALARY"))
+MAX_SALARY = int(os.getenv("MAX_SALARY"))
 
-MINIMAL_SALARY = 3000
-MAX_SALARY = 4500
-
-DJINNI_LOGIN = os.getenv('DJINNI_LOGIN')
-DJINNI_PASSWORD = os.getenv('DJINNI_PASSWORD')
+DJINNI_LOGIN = os.getenv("DJINNI_LOGIN")
+DJINNI_PASSWORD = os.getenv("DJINNI_PASSWORD")
